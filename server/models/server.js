c641b139738480fae17271e2c2afcc7ffe212508
tkeatkaew <thanit@rmutl.ac.th>
2025-01-15 08:45:48 +0700

    server https updated and almost done

--- server/models/server.js
+++ server/models/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-
+const https = require('https');
+const fs = require('fs');
 
 require('dotenv').config();
 
@@ -11,8 +12,27 @@ console.log(process.env.PORT);
 
 class Server {
   constructor() {
+
+    if (process.env.NODE_ENV == "development"){
+      this.key = "server.key";
+      this.cert = "server.crt";
+    }
+    else{
+      this.key = "/etc/ssl/server.key";
+      this.cert = "/etc/ssl/server.crt";
+    }
+
+    // Load SSL Certificates
+    const sslOptions = {
+      key: fs.readFileSync(this.key), // Replace with your SSL key file path
+      cert: fs.readFileSync(this.cert), // Replace with your SSL certificate file path
+    };
+
     this.app = express();
-    this.port = process.env.PORT || 8080;
+    this.port = process.env.PORT || 8080; // HTTP port
+    this.httpsPort = process.env.HTTPS_PORT || 8081; // HTTPS port
+    this.sslOptions = sslOptions;
+
     this.paths = {
       auth: "/api/auth",
       homepage: "/api/homepage",
@@ -43,12 +63,23 @@ class Server {
       );
     });
   }
-
+/*
   listen() {
     this.app.listen(this.port, () => {
       console.log("Server running on port: ", this.port);
     });
   }
+*/
+
+listen() {
+
+  // Start HTTPS server
+  https.createServer(this.sslOptions, this.app).listen(this.httpsPort, () => {
+    console.log(`HTTPS Server running on port: ${this.httpsPort}`);
+  });
+}
+
+
 }
 
 module.exports = Server;
